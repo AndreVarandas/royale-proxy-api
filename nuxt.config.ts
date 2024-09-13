644e025673f017cd6c628598fd1afe110dd7e45f
André Varandas <andre.m.varandas@gmail.com>
2024-09-13 15:59:05 +0100

    Refactor devtools configuration and add compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV === "development" },
+  compatibilityDate: "2024-09-13",
 
   runtimeConfig: {
     CLASH_ROYALE_API_TOKEN: process.env.CLASH_ROYALE_API_TOKEN,
